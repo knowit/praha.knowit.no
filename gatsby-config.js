1d0ed304dbb1aafe5530cc1d85e24e5296da5e48
Christian Peter <christian150790@gmail.com>
2019-09-29 19:26:45 +0200

    Adds gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-emotion',
-
+    'gatsby-plugin-material-ui',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
